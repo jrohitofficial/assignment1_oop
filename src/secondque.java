f4c54d6d1331f44260d21e72460c77e1d98001ef
BisKIRAN_RJ <bewithu.rj@gmail.com>
2021-12-06 11:32:40 +0545

    palimdrome

---
+++ src/secondque.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class secondque {
+
+
+        public static void main(String args[])
+        {
+            String str, rev = "";
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Enter a string:");
+            str = sc.nextLine();
+
+            int length = str.length();
+
+            for ( int i = length - 1; i >= 0; i-- )
+                rev = rev + str.charAt(i);
+
+            if (str.equals(rev))
+                System.out.println(str+" is a palindrome");
+            else
+                System.out.println(str+" is not a palindrome");
+
+        }
+    }
+
+
